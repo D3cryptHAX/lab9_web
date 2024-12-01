a781977977e60d1f065382e48681f72329b38db1
Матвійчук Віталій <81376426+D3cryptHAX@users.noreply.github.com>
2024-12-01 19:21:42 +0200

    Add files via upload

---
+++ src/Expenses.js
@@ -0,0 +1,50 @@
+import React, { useState } from "react";
+import "./Expenses.css";
+import ExpensesFilter from "./ExpensesFilter";
+import ExpensesList from "./ExpensesList";
+import Chart from "./Chart";
+
+const Expenses = (props) => {
+  const [filteredYear, setFilteredYear] = useState("2021");
+
+  const filterChangeHandler = (selectedYear) => {
+    setFilteredYear(selectedYear);
+  };
+
+  const filteredExpenses = props.items.filter((expense) => {
+    return expense.date.getFullYear().toString() === filteredYear;
+  });
+
+  const chartDataPoints = [
+    { label: "Jan", value: 0 },
+    { label: "Feb", value: 0 },
+    { label: "Mar", value: 0 },
+    { label: "Apr", value: 0 },
+    { label: "May", value: 0 },
+    { label: "Jun", value: 0 },
+    { label: "Jul", value: 0 },
+    { label: "Aug", value: 0 },
+    { label: "Sep", value: 0 },
+    { label: "Oct", value: 0 },
+    { label: "Nov", value: 0 },
+    { label: "Dec", value: 0 },
+  ];
+
+  filteredExpenses.forEach((expense) => {
+    const expenseMonth = expense.date.getMonth(); // January = 0
+    chartDataPoints[expenseMonth].value += expense.amount;
+  });
+
+  return (
+    <div className="expenses">
+      <ExpensesFilter
+        selected={filteredYear}
+        onChangeFilter={filterChangeHandler}
+      />
+      <Chart dataPoints={chartDataPoints} />
+      <ExpensesList items={filteredExpenses} />
+    </div>
+  );
+};
+
+export default Expenses;
